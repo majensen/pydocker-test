a58629f1cffacfda367b3add3da75531e3cfeaa5
Mark A. Jensen <mark.jensen@nih.gov>
2024-10-13 21:47:32 -0400

    add back check extend timeout 120s

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -25,8 +25,8 @@ def mdb_versioned(docker_services, docker_ip):
     bolt_url = "bolt://{}:{}".format(docker_ip, bolt_port)
     http_url = "http://{}:{}".format(docker_ip, http_port)
     sleep(wait)
-    # docker_services.wait_until_responsive(
-    #     timeout=60.0, pause=1.0, check=lambda: is_responsive(http_url)
-    # )
+    docker_services.wait_until_responsive(
+        timeout=120.0, pause=1.0, check=lambda: is_responsive(http_url)
+    )
     return (bolt_url, http_url)
 
